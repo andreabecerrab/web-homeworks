51142addc60cc88c3a947228c0ce39b18e5f009d
andreabecerrab <andii1wiki@gmail.com>
2021-02-23 12:37:54 -0600

    style

--- resources/views/guides/create.blade.php
+++ resources/views/guides/create.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.main')
 @section('content')
 
+<div style="padding: 2rem;width:500px;">
+    <h1>Create your guide here!</h1>
+    <div>
+        <form action="{{ route('guides.store') }}" method="POST">
+        @csrf
+        <label for="">Nombre</label><br>
+        <input type="text" name="name"  class="form-control"><br>
+        <label for="">Tipo</label><br>
+        <input type="text" name="type"  class="form-control"><br>
+        <label for="">Descripcion</label><br>
+        <input type="text" name="description"  class="form-control"><br>
 
-<h1>Create your guide here!</h1>
-<div>
-    <form action="{{ route('guides.store') }}" method="POST">
-    @csrf
-    <label for="">Nombre</label>
-    <input type="text" name="name">
-    <label for="">Tipo</label>
-    <input type="text" name="type">
-    <label for="">Descripcion</label>
-    <input type="text" name="description"><br>
+        <label for="" class="mt-2">Select products for this guide:</label><br>
+        @foreach ($products as $item)
+            <input type="checkbox" value="{{ $item->id }}" name="products[]">
+            <label for="{{ $item->id }}"> {{ $item->name }}</label><br>
+        @endforeach
 
-    @foreach ($products as $item)
-        <input type="checkbox" value="{{ $item->id }}" name="products[]">
-        <label for="{{ $item->id }}"> {{ $item->name }}</label><br>
-    @endforeach
-
-    <input type="submit" value="Guardar">
-</form>
+        <input type="submit" value="Save" style="margin-top:2rem;" class="btn btn-success">
+    </form>
+    </div>
 </div>
 
 @endsection
