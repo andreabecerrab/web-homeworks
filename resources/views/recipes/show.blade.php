81a3038350008611a969e969b9fe5cf6575cb3fd
liaDiaz <46771468+liaDiaz@users.noreply.github.com>
2021-02-25 17:33:49 -0600

    diseño2

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>Recipes </h1>
-<h2>{{ $recipe->recipe }}</h2>
+
+
+<div class="container-fluid">
+    <h1 class="text-center mx-auto" >{{ $recipe->name }}</h1>
+    <br>
+    <div class="d-flex justify-content-center row ">
+  <div class="font-weight-bold col-9 ">Ingredientes extra:<br> <br>{{$recipe->ingredients}}</div>
+  
+  <div class="col-9 font-weight-bold"> Instrucciones :<br><br> {{ $recipe->body}}</div>
+  
+</div>
+
+<div>
+
+</div>
+
+
+</div>
+
+
 
 @endsection
